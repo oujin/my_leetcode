218e0333ea80662f88efb25b9b661f71dee4203c
oujin <1354100681@qq.com>
2020-11-16 01:41:27 +0800

    update

--- 160.相交链表.py
+++ 160.相交链表.py
@@ -1,12 +1,20 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+#
+# @lc app=leetcode.cn id=160 lang=python3
+#
+# [160] 相交链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         if headA is None or headB is None:
-            return False
+            return None
         p1, p2 = headA, headB
         n1, n2 = 0, 0
         while p1.next:
@@ -29,24 +37,7 @@ class Solution:
                 return p1
             p1, p2 = p1.next, p2.next
         return None
-     
+            
 
-n0 = ListNode(4)
-n1 = ListNode(1)
-n2 = ListNode(8)
-n3 = ListNode(4)
-n4 = ListNode(5)
-n5 = ListNode(5)
-n6 = ListNode(6)
-n7 = ListNode(1)
-n0.next = n1
-n1.next = n2
-n2.next = n3
-n3.next = n4
-n5.next = n6
-n6.next = n7
-n7.next = n2
-s = Solution()
-node = s.getIntersectionNode(n0, n5)
-# print(node, n5)
-print(node.val)
+# @lc code=end
+
