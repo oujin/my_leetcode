4d2baf921d60f75b3843e9b1c09c44245c9694f3
oujin <1354100681@qq.com>
2021-04-15 17:26:04 +0800

    update

--- 459.重复的子字符串.py
+++ 459.重复的子字符串.py
@@ -7,23 +7,25 @@
 # @lc code=start
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        c_set = set(s)
-        n_total, n_unique = len(s), len(c_set)
-        if n_unique > n_total / 2:
-            return False
-        mask_set = set()
-        for i in range(n_total // 2):
-            mask_set.add(s[i])
-            if len(mask_set) < n_unique or (n_total % (i + 1)):
-                continue
-            is_repeated = True
-            for j in range(1, n_total // (i + 1)):
-                if s[(i+1)*j:(i+1)*(j+1)] != s[:i+1]:
-                    is_repeated = False
-                    break
-            if is_repeated:
-                return True
-        return False
+        # c_set = set(s)
+        # n_total, n_unique = len(s), len(c_set)
+        # if n_unique > n_total / 2:
+        #     return False
+        # mask_set = set()
+        # for i in range(n_total // 2):
+        #     mask_set.add(s[i])
+        #     if len(mask_set) < n_unique or (n_total % (i + 1)):
+        #         continue
+        #     is_repeated = True
+        #     for j in range(1, n_total // (i + 1)):
+        #         if s[(i+1)*j:(i+1)*(j+1)] != s[:i+1]:
+        #             is_repeated = False
+        #             break
+        #     if is_repeated:
+        #         return True
+        # return False
+        # 法二
+        return (s + s)[1:-1].__contains__(s)
             
 
 
